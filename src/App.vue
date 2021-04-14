2982e9629c0ccb078dadbb37bcc91119d8abbd07
Pablo Martínez <martipev2@hotmail.com>
2021-04-14 03:27:44 -0500

    Primeros selects

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,97 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <SelectsWrapper>
+    <Select
+      v-model="hecho"
+      :id="'select1'"
+      :label="'Hecho o agresión'"
+      :options="[
+        {
+          id: 1,
+          name: 'Test1',
+        },
+      ]"
+    />
+    <Select
+      v-model="estado"
+      :id="'select2'"
+      :label="'Estado'"
+      :options="[
+        {
+          id: 1,
+          name: 'Test1',
+        },
+        {
+          id: 2,
+          name: 'Test2',
+        },
+      ]"
+    />
+    <Select
+      v-model="municipio"
+      :id="'select3'"
+      :label="'Municipio'"
+      :options="[
+        {
+          id: 1,
+          name: 'Test1',
+        },
+        {
+          id: 2,
+          name: 'Test2',
+        },
+        {
+          id: 3,
+          name: 'Test3',
+        },
+      ]"
+      @change="onChange"
+    />
+  </SelectsWrapper>
 </template>
 
-<script>
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+import styled from "vue3-styled-components";
 import HelloWorld from "./components/HelloWorld.vue";
+import Select from "./components/Select.vue";
 
-export default {
-  name: "App",
+const SelectsWrapper = styled.section`
+  display: flex;
+  flex-flow: row wrap;
+`;
+
+@Options({
   components: {
     HelloWorld,
+    Select,
+    SelectsWrapper,
   },
-};
+})
+export default class App extends Vue {
+  hecho: string = "";
+  estado: string = "";
+  municipio: string = "";
+
+  mounted() {
+    if (this.hecho === "" && this.estado === "" && this.municipio === "") {
+      alert("aló");
+    }
+  }
+
+  onChange() {
+    console.log(this.hecho, this.estado, this.municipio);
+  }
+}
 </script>
 
 <style>
+* {
+  box-sizing: border-box;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: Open Sans, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/Select.vue
@@ -0,0 +1,85 @@
+<template>
+  <SelectWrapper class="select-wrapper">
+    <SelectLabel v-bind:for="id">{{ !!label ? label : "" }}</SelectLabel>
+    <Select v-model="selectedOption" v-bind:id="id" @change="onChange">
+      <option value="" selected disabled>
+        {{ !!placeholder ? placeholder : "Seleccione una opción" }}
+      </option>
+      <option v-for="opt in options" v-bind:value="opt.id" v-bind:key="opt.id">
+        {{ opt.name }}
+      </option>
+    </Select>
+  </SelectWrapper>
+</template>
+
+<script lang="ts">
+// Librerías
+import { PropType } from "vue";
+import { Options, Vue } from "vue-class-component";
+import styled from "vue3-styled-components";
+
+// Interfaces
+import { Option } from "../interfaces/interfaces";
+
+const SelectWrapper = styled.div`
+  display: flex;
+  flex-flow: column nowrap;
+  max-width: 400px;
+  padding-left: 5px;
+`;
+
+const SelectLabel = styled.label`
+  font-size: 12px;
+  font-weight: bold;
+  color: #646464;
+  padding-bottom: 5px;
+`;
+
+const Select = styled.select`
+  padding: 2px 5px;
+  font-size: 14px;
+  color: #1e1e1e;
+  border: 2px solid #1e1e1e;
+  border-radius: 5px;
+  background: transparent;
+  background-image: url("data:image/svg+xml;utf8,<svg fill='black' height='24' viewBox='0 0 24 24' width='24' xmlns='http://www.w3.org/2000/svg'><path d='M7 10l5 5 5-5z'/><path d='M0 0h24v24H0z' fill='none'/></svg>");
+  background-repeat: no-repeat;
+  background-position-x: 100%;
+  background-position-y: 50%;
+  appearance: none;
+  min-width: 200px;
+`;
+
+@Options({
+  props: {
+    id: {
+      type: String,
+      required: true,
+    },
+    label: {
+      type: String,
+      required: false,
+    },
+    options: {
+      type: Array as PropType<Option[]>,
+      required: true,
+    },
+    placeholder: {
+      type: String,
+      required: false,
+    },
+  },
+  components: {
+    SelectWrapper,
+    Select,
+    SelectLabel,
+  },
+})
+export default class SelectComponent extends Vue {
+  selectedOption: string = "";
+
+  onChange() {
+    console.log(this.selectedOption);
+  }
+}
+</script>
